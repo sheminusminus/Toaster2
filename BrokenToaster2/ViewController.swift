ed08b55f7357770429b9f2c21a3bfcd13bf711bd
Emily Kolar <emkolar@gmail.com>
2017-07-11 06:44:52 -0500

    update toaster

--- BrokenToaster2/ViewController.swift
+++ BrokenToaster2/ViewController.swift
@@ -9,17 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+	
+	//---------------------------
+	// MARK: properties
+	//---------------------------
+	
+	// data we will get from user
+	var name: String!
+	
+	// ui (storyboard objects)
+	@IBOutlet weak var userInput: UITextField!
 
+	//---------------------------
+	// MARK: lifecycle functions
+	//---------------------------
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
 	}
-
+	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
 	}
+	
+	//---------------------------
+	// MARK: actions (sent by storyboard objects)
+	//---------------------------
 
+	@IBAction func donePressed(_ sender: Any) {
+		
+		name = userInput.text!
+		
+		// get a reference to our storyboard
+		let storyboard = UIStoryboard(name: "Main", bundle: nil)
+		
+		// create a ToasterViewController in memory, using a reference to our storyboard view controller
+		let toasterVC = storyboard.instantiateViewController(withIdentifier: "toasterVC") as! ToasterViewController
+		
+		// set the user's name variable inside our toasterVC so it knows that information
+		toasterVC.userName = self.name
+		
+		// present the toaster view controller graphically, in the ui
+		self.present(toasterVC, animated: true, completion: nil)
+	}
 
 }
 
